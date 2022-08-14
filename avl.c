13ee2e6e87454a689b36472fb1be26bac7ffdc05
tejas <tejasshah@gmail.com>
2022-08-14 19:17:32 +0530

    avl rotation function and balance factor

--- avl.c
+++ avl.c
@@ -40,7 +40,46 @@ int calculateHeight(struct node* node){//10
         return 1+rh;//1+2 =>  3
     }
 }
+
+int calculateBalanceFactor(struct node *root){
+        int lh =0,rh=0;
+        if(root == NULL){
+            return 0;
+        }
+        if(root->left != NULL){
+            lh = root->left->height;
+        }
+        if(root->right != NULL){
+            rh = root->right->height;
+        }
+
+        return lh - rh;
+}
+struct node* rightRotate(struct node *node){
+    struct node *nodeleft,*nodeleftright;
+    nodeleft = node->left;
+    nodeleftright = node->left->right;
+
+    nodeleft->right = node;
+    node->left = nodeleftright;
+    return nodeleft;
+}
+
+
+struct node* leftRotate(struct node *node){
+    struct node *noderight,*noderightleft;
+    noderight = node->right;
+    noderightleft = node->right->left;
+
+    noderight->left = node;
+    node->right = noderightleft;
+    return noderight;
+}
+
+
+
 struct node* insertNode(struct node *node,int data){// (10,30) => (20,30) =>  (NULL,30)
+    int bf;
     if(node == NULL){
         return createNode(data);
     }
@@ -52,15 +91,62 @@ struct node* insertNode(struct node *node,int data){// (10,30) => (20,30) =>  (N
         //20->right =30;
     }
     node->height = calculateHeight(node); //10
+    bf = calculateBalanceFactor(node);
+    printf(" %d(%d) ",node->data,bf);
+    if( !(bf >-2 && bf < 2) ){
+        printf("***Rotation**** %d",node->data);
+        if(bf < 0 && node->right->data < data ){
+            printf(" RR ");
+
+        }else if(bf < 0 && node->right->data > data){
+            printf(" RL ");
+        }else if(bf > 0 && node->left->data > data){
+            printf(" LL ");
+        }else if(bf > 0 && node->left->data < data){
+            printf(" LR ");
+        }
+    }
+
+
     return node;
 }
 
+void inOrder(struct node *root){
+    if(root==NULL)
+        return;
+    inOrder(root->left);
+    printf(" %d(%d) ",root->data,root->height);
+    inOrder(root->right);
+}
+
 int main(){
+           printf ("\nInserting 100");
+    root = insertNode(root,100);
 
-    root = insertNode(root,10);
-           insertNode(root,20);
-           insertNode(root,30);//(10,30)
+         // printf ("\nInserting 150");
+          // insertNode(root,150);
 
+
+            //printf ("\nInserting 300");
+           //insertNode(root,300);
+
+           printf ("\nInserting 50");
+          insertNode(root,50);//(10,30)
+
+           //printf ("\nInserting 30");
+           //insertNode(root,30);//
+
+            //printf ("\nInserting 20");
+           //insertNode(root,20);//
+
+         //  printf ("\nInserting 125");
+         //  insertNode(root,125);
+
+            printf ("\nInserting 90");
+            insertNode(root,90);
+
+            printf("\nTREE\n");
+           inOrder(root);
 
     return 0;
 }
